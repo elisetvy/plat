8df4ef9eab721d27626edf3de15e1073803a9b0c
Elise <elisetvy@gmail.com>
2023-05-24 21:00:54 -0700

    Add cart functionality

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ for (let i = 0; i < collapsible.length; i++) {
     }
   });
 }
+
+const numberOfTickets = document.getElementById("numberOfTickets");
+const decreaseButton = document.getElementById("decrease");
+const increaseButton = document.getElementById("increase");
+const itemTotal = document.getElementById("item-total");
+const cartTotal = document.getElementById("cart-total");
+
+decreaseButton.addEventListener("click", () => {
+  let currentNum = parseInt(numberOfTickets.textContent);
+  if (currentNum > 0) {
+    currentNum--;
+    numberOfTickets.textContent = currentNum;
+    itemTotal.textContent = "$" + currentNum * 10 + ".00";
+    cartTotal.textContent = itemTotal.textContent;
+  }
+});
+
+increaseButton.addEventListener("click", () => {
+  let currentNum = parseInt(numberOfTickets.textContent);
+  if (currentNum < 100) {
+    currentNum++;
+    numberOfTickets.textContent = currentNum;
+    itemTotal.textContent = "$" + currentNum * 10 + ".00";
+    cartTotal.textContent = itemTotal.textContent;
+  }
+});
