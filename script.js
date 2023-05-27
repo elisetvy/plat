c028cf44e1f268458289ba5fc3a963d454f0b049
Elise <elisetvy@gmail.com>
2023-05-27 12:53:52 -0700

    Add cart total functionality

--- script.js
+++ script.js
@@ -12,28 +12,79 @@ for (let i = 0; i < collapsible.length; i++) {
   });
 }
 
-const numberOfTickets = document.getElementById("numberOfTickets");
-const decreaseButton = document.getElementById("decrease");
-const increaseButton = document.getElementById("increase");
-const itemTotal = document.getElementById("item-total");
+// const numberOfTickets = document.getElementById("numberOfTickets");
+// const decreaseButton = document.getElementById("decrease");
+// const increaseButton = document.getElementById("increase");
+// const itemTotal = document.getElementById("item-total");
+// const cartTotal = document.getElementById("cart-total");
+
+// decreaseButton.addEventListener("click", () => {
+//   let currentNum = parseInt(numberOfTickets.textContent);
+//   if (currentNum > 0) {
+//     currentNum--;
+//     numberOfTickets.textContent = currentNum;
+//     itemTotal.textContent = "$" + currentNum * 10 + ".00";
+//     cartTotal.textContent = itemTotal.textContent;
+//   }
+// });
+
+// increaseButton.addEventListener("click", () => {
+//   let currentNum = parseInt(numberOfTickets.textContent);
+//   if (currentNum < 100) {
+//     currentNum++;
+//     numberOfTickets.textContent = currentNum;
+//     itemTotal.textContent = "$" + currentNum * 10 + ".00";
+//     cartTotal.textContent = itemTotal.textContent;
+//   }
+// });
+
+const cartTable = document.querySelector(".cart-table");
+
 const cartTotal = document.getElementById("cart-total");
 
-decreaseButton.addEventListener("click", () => {
-  let currentNum = parseInt(numberOfTickets.textContent);
-  if (currentNum > 0) {
-    currentNum--;
-    numberOfTickets.textContent = currentNum;
-    itemTotal.textContent = "$" + currentNum * 10 + ".00";
-    cartTotal.textContent = itemTotal.textContent;
+const itemTotalCells = document.querySelectorAll("#item-total");
+
+cartTable.addEventListener("click", function (event) {
+  const target = event.target;
+  if (target.id === "increase") {
+    const num = target.parentNode.querySelector("#numberOfTickets");
+    incrementNumber(num);
+    let itemTotal = target.parentNode.nextElementSibling;
+    let itemPrice = +num.textContent * 10;
+    itemTotal.textContent = "$" + itemPrice + ".00";
   }
+  if (target.id === "decrease") {
+    const num = target.parentNode.querySelector("#numberOfTickets");
+    decrementNumber(num);
+    let itemTotal = target.parentNode.nextElementSibling;
+    let itemPrice = +num.textContent * 10;
+    itemTotal.textContent = "$" + itemPrice + ".00";
+  }
+
+  let dataArray = [];
+
+  itemTotalCells.forEach(function (cell) {
+    const data = parseInt(cell.textContent.slice(1));
+    dataArray.push(data);
+  });
+
+  dataArray = dataArray.reduce((x, y) => x + y);
+
+  cartTotal.textContent = "$" + dataArray + ".00";
 });
 
-increaseButton.addEventListener("click", () => {
-  let currentNum = parseInt(numberOfTickets.textContent);
-  if (currentNum < 100) {
-    currentNum++;
-    numberOfTickets.textContent = currentNum;
-    itemTotal.textContent = "$" + currentNum * 10 + ".00";
-    cartTotal.textContent = itemTotal.textContent;
+function incrementNumber(element) {
+  let number = parseInt(element.textContent);
+  if (number < 100) {
+    number++;
+    element.textContent = number;
   }
-});
+}
+
+function decrementNumber(element) {
+  let number = parseInt(element.textContent);
+  if (number > 0) {
+    number--;
+    element.textContent = number;
+  }
+}
